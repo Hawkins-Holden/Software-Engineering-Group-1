cbbd8824429cc70cc7375212acf1f9f1eaa8551a
Anjan Pandey <pandeya1@warhawks.ulm.edu>
2017-02-27 12:32:58 -0600

    First Prototype

---
+++ First Prototype/src/application/Basic.java
@@ -0,0 +1,19 @@
+package application;
+
+//import java.util.*;
+//import java.io.*;
+
+public class Basic
+{
+	
+	String zip = new String();
+	
+	public void setZip(String newZip)
+	{
+		this.zip = newZip;
+	}
+	public String getZip()
+	{
+		return this.zip;
+	}
+}
