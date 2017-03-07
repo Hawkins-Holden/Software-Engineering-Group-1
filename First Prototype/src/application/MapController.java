f9c3733354b19221251e904be8450c60ccae6b51
Brent Cole <coleb@warhawks.ulm.edu>
2017-03-07 10:53:36 -0600

    Add files via upload

---
+++ First Prototype/src/application/MapController.java
@@ -0,0 +1,21 @@
+package application;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+import javafx.stage.*;
+import javafx.scene.control.TextField;
+import javafx.scene.web.WebView;
+
+public class MapController  implements Initializable{
+
+	
+	
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+	}
+
+}
