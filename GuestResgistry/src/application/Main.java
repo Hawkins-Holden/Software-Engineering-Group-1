e2426a61fb5f97ddc3e447e4a4645dec773b136a
ConnorDixonWarhawks <dixonc@warhawks.ulm.edu>
2017-03-21 11:38:19 -0500

    Aaron
    new version of main, calls controller and decides which fxml file is
    loaded based on whether this is the map or form

--- GuestResgistry/src/application/Main.java
+++ GuestResgistry/src/application/Main.java
@@ -1,88 +1,77 @@
 package application;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.geometry.Insets;
-import javafx.scene.Group;
-import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
+import javafx.scene.*;
+import javafx.scene.control.*;
+import javafx.stage.*;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
-import javafx.scene.control.Button;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 
-// w w  w .jav a  2 s . c o  m
-@SuppressWarnings("restriction")
-public class Main extends Application
-{
-	
-	public Visitor getVisitor()
-	{
-        int VisitorID = (int) Math.ceil((Math.random()*10000));
-        Visitor visitor = new Visitor(VisitorID, "Hawkins", "W", "Holden", "hholdengold@gmail.com", true, "Pleasure", "32", "-92");//just something random to use for testing
-		return visitor;
+public class Main extends Application {
+
+	/** Intentionally start with map, this will be changed later. */
+	private static String FxmlTarget = "MapViewer.fxml";
+	/**Is the form (as opposed to the map) to be loaded?*/
+	private static boolean isForm = false;
+
+	public static void main(String[] args) {
+		launch(args);
 	}
-	
-  public static void main(String[] args) 
-  {
-    launch(args);
-  }
-  @Override
-  public void start(Stage stage) 
-  {
-    Scene scene = new Scene(new Group(), 450, 250);
 
-    //make input field
-    TextField notification = new TextField ();
-    //notification.setText("Input");
-    
-    //make output field
-    TextField output = new TextField ();
-    //output.setText("Your ZIP is:");
-    
-    //make button
-    Button btn = new Button();
-    btn.setText("Submit");
-    
-    //object declaration
-    
-    notification.clear();
-    
-    GridPane grid = new GridPane();
-    grid.setVgap(4);
-    grid.setHgap(10);
-    grid.setPadding(new Insets(5, 5, 5, 5));
-    grid.add(new Label("ID:"), 0, 0);
-    grid.add(notification, 1, 0);
-    grid.add(btn, 15, 0);
-    grid.add(output, 1, 10);
-    grid.add(new Label("Your ID is:"), 0, 10);
-    
+	@Override
+	public void start(Stage primaryStage) throws Exception {
 
-    
-    Group root = (Group) scene.getRoot();
-    root.getChildren().add(grid);
-    stage.setScene(scene);
-    stage.show();
-    
-    btn.setOnAction(new EventHandler<ActionEvent>() 
-    {
-    	 
-        @Override
-        public void handle(ActionEvent event) 
-        {
-        	Integer id = Integer.parseInt(notification.getText());
-            System.out.println("testing 1...");
-           
-            Visitor x = getVisitor();
+		//Check which fxml file is to be loaded.
+		if (isForm = false) {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource(FxmlTarget));
+			Parent root = loader.load();
+			//MapController controls = loader.getController();
+			primaryStage.setTitle("Guest Book");
+			primaryStage.setScene(new Scene(root, 1024, 680));
+			primaryStage.show();
+		}
+		if (isForm = true) {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource(FxmlTarget));
+			Parent root = loader.load();
+			//FormController controls = loader.getController();
+			primaryStage.setTitle("Guest Form");
+			primaryStage.setScene(new Scene(root, 1024, 680));
+			primaryStage.show();
+		}
+		
+		MapController.setMain(this);
 
-            JDBC.insertIntoDB(x.getId());
-            System.out.println("testing 2...");
-            id = JDBC.readFromDB(x.getId());
-            System.out.println(id);
-            output.setText(id.toString());
-        }
-    });
-  }
+	}
+
+	/**
+	 * Intentionally private
+	 * 
+	 * @return
+	 */
+	private String getTarget() {
+		return FxmlTarget;
+	}
+
+	public void setTarget(String newTarg) throws Exception {
+		//Make a new stage so start() can be called again.
+		Stage stage = new Stage();
+		
+		//Set the target to the form.fxml or whatever is needed.
+		FxmlTarget = newTarg;
+		
+		//Set the flag so the correct controlle (FormController.java) is loaded.
+		isForm = true;
+		start(stage);
+	}
+
+	public Visitor getVisitor() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
 }
