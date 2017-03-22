01326d8a350ea78c43143cb02fd167ee60a3e091
ConnorDixonWarhawks <dixonc@warhawks.ulm.edu>
2017-03-22 13:26:16 -0500

    Excel Report Generation - Version 1
    JXL works now. We can make comprehensive reports, or just generate email
    lists

--- GuestResgistry/src/adminApplication/AdminController.java
+++ GuestResgistry/src/adminApplication/AdminController.java
@@ -24,8 +24,8 @@ public class AdminController implements Initializable {
 
 	}
 
-	public void write() throws IOException, WriteException {
-		File file = new File("excelReport.xls");
+	public void generateMailingList() throws IOException, WriteException {
+		File file = new File("mailingList.xls");
 		
 		WritableWorkbook myexcel = Workbook.createWorkbook(file);
 		WritableSheet mysheet = myexcel.createSheet("mySheet", 0);
@@ -51,6 +51,42 @@ public class AdminController implements Initializable {
 		myexcel.close();
 	}
 	
+	public void generateReport() throws IOException, WriteException {
+		File file = new File("comprehensiveReport.xls");
+		
+		WritableWorkbook myexcel = Workbook.createWorkbook(file);
+		WritableSheet mysheet = myexcel.createSheet("mySheet", 0);
+
+		List<List<String>> dataTable = AdminJDBC.generateReport();
+		
+		mysheet.addCell(new Label(0, 0, "This report was generated on " + getDate() + " at " + getTime() + "."));
+		
+		mysheet.addCell(new Label(0, 1, "VisitorID"));
+		mysheet.addCell(new Label(1, 1, "First"));
+		mysheet.addCell(new Label(2, 1, "MI"));
+		mysheet.addCell(new Label(3, 1, "Last"));
+		mysheet.addCell(new Label(4, 1, "Email"));
+		mysheet.addCell(new Label(5, 1, "From (Latitude)"));
+		mysheet.addCell(new Label(6, 1, "From (Longitude)"));
+		mysheet.addCell(new Label(7, 1, "Number in Party"));
+		mysheet.addCell(new Label(8, 1, "How Referred"));
+		mysheet.addCell(new Label(9, 1, "Stayed in M/WM Hotel"));
+		mysheet.addCell(new Label(10, 1, "Destination"));
+		mysheet.addCell(new Label(11, 1, "Repeat Visitor?"));
+		mysheet.addCell(new Label(12, 1, "Reason For Travelling"));
+		mysheet.addCell(new Label(13, 1, "Date of Visit"));
+		
+		for(int i = 0; i < dataTable.size(); i++)
+		{
+			for (int j = 0; j < dataTable.get(0).size(); j++)
+			{
+				mysheet.addCell(formatData(i, j, dataTable.get(i).get(j)));
+			}
+		}
+		myexcel.write();
+		myexcel.close();
+	}
+	
 	private Label formatData(int i, int j, String data)
 	{
 		return new Label(j, i+2, data);
@@ -63,7 +99,7 @@ public class AdminController implements Initializable {
 
 	private String getDate() {
 		Calendar timestamp = Calendar.getInstance();
-		return timestamp.get(Calendar.MONTH) + "/" + timestamp.get(Calendar.DATE) + "/" + timestamp.get(Calendar.YEAR);
+		return (timestamp.get(Calendar.MONTH) + 1) + "/" + timestamp.get(Calendar.DATE) + "/" + timestamp.get(Calendar.YEAR);
 	}
 
 }
--- GuestResgistry/src/adminApplication/AdminJDBC.java
+++ GuestResgistry/src/adminApplication/AdminJDBC.java
@@ -33,14 +33,14 @@ public class AdminJDBC {
 
 				// create and select db
 
-				stmt.execute("CREATE DATABASE IF NOT EXISTS VisitorsDB");
-				stmt.execute("USE VisitorsDB");
+				stmt.execute("CREATE DATABASE IF NOT EXISTS visitordb");
+				stmt.execute("USE visitordb");
 
-				/**
+ 				/**
 				 * Query entries with the Zip '71467'
 				 */
 
-				ResultSet res = stmt.executeQuery("SELECT * FROM visitors WHERE Email IS NOT NULL");
+				ResultSet res = stmt.executeQuery("SELECT * FROM visitors WHERE Email IS NOT NULL ORDER BY VisitorID");
 
 				/**
 				 * Iterate over the result set from the above query
@@ -53,6 +53,90 @@ public class AdminJDBC {
 					currentResult.add((String) res.getObject("MiddleInitial"));
 					currentResult.add(res.getString("Lname"));
 					currentResult.add(res.getString("Email"));
+					
+					resultSet.add(currentResult);
+				}
+
+			}
+			
+			return resultSet;
+		}
+
+		catch (Exception e) {
+			System.err.println("Exception: " + e.getMessage());
+		} finally {
+			try {
+				if (con != null)
+					con.close();
+			} catch (SQLException e) {
+			}
+		}
+		return null;
+	}
+
+	public static List<List<String>> generateReport() {
+		Connection con = null;
+		Statement stmt;
+		
+		List<List<String>> resultSet = new ArrayList<List<String>>();
+
+		String url = "jdbc:mysql://localhost:3306/test";
+		String user = "root";
+		String password = "";
+
+		try {
+			Class.forName("com.mysql.jdbc.Driver").newInstance();
+			con = DriverManager.getConnection(url, user, password);
+
+			if (!con.isClosed()) {
+				System.out.println("Successfully connected to " + "MySQL server using TCP/IP...");
+				stmt = con.createStatement();
+
+				// create and select db
+
+				stmt.execute("CREATE DATABASE IF NOT EXISTS visitordb");
+				stmt.execute("USE visitordb");
+				/**
+				 * Query entries with the Zip '71467'
+				 */
+
+				//ResultSet res = stmt.executeQuery("SELECT * FROM visitors NATURAL JOIN visits NATURAL JOIN visitorlocations ORDER BY Visiting_Day");
+				ResultSet res = stmt.executeQuery("SELECT * FROM visitors LEFT JOIN visits ON visitors.VisitorID = visits.VisitorID LEFT JOIN visitorlocations on visitors.VisitorID = visitorlocations.VisitorID ORDER BY Visiting_Day");
+				
+				/**
+				 * Iterate over the result set from the above query
+				 */
+				while (res.next()) {
+					List<String> currentResult = new ArrayList<String>();
+					
+					currentResult.add(((Integer) res.getInt("VisitorID")).toString());
+					currentResult.add(res.getString("Fname"));
+					currentResult.add((String) res.getObject("MiddleInitial"));
+					currentResult.add(res.getString("Lname"));
+					currentResult.add(res.getString("Email"));
+					currentResult.add(res.getString("Latitude"));
+					currentResult.add(res.getString("Longitude"));
+					currentResult.add(((Integer) res.getInt("Party")).toString());
+					currentResult.add(res.getString("Heard"));
+					if (res.getInt("Hotel") == 1)
+					{
+						currentResult.add("Yes");
+					}
+					else
+					{
+						currentResult.add("No");
+					}
+					currentResult.add(res.getString("Destination"));
+					if (res.getInt("RepeatVisit") == 1)
+					{
+						currentResult.add("Yes");
+					}
+					else
+					{
+						currentResult.add("No");
+					}
+					currentResult.add(res.getString("TravelingFor"));
+					currentResult.add(res.getString("Visiting_Day"));	
 					
 					resultSet.add(currentResult);
 				}
