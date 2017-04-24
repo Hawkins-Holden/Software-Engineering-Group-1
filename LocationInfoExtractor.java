b8a736b1b2886c4c5c5bc95353b0989f2c85f7b8
Brent Cole <coleb@warhawks.ulm.edu>
2017-04-24 17:08:32 -0500

    Add files via upload

--- LocationInfoExtractor.java
+++ LocationInfoExtractor.java
@@ -9,19 +9,20 @@ public class LocationInfoExtractor {
 	 * @param rawInfo
 	 * @return
 	 */
-	public static String[] extractLocationInfo(String rawInfo){
+	public static String[] extractLocationInfo(String[] rawInfo){
 		
 		int i = 0;
 		String zipCode = "";
 		String state = "";
+		String metro = rawInfo[0];
 		String[] reformatedInfo = new String[15]; //Intentionally larger than necessary
 		
-		Scanner scan = new Scanner(rawInfo);
+		Scanner scan = new Scanner(rawInfo[1]);
 		scan.useDelimiter(", ");
 		
 		//This will put the street address in the first spot [0], city second, state/zip third, and the nation last
 		//Only applicable to the US, so that must be found out as well
-		if (rawInfo.contains("USA")) {
+		if (rawInfo[1].contains("USA")) {
 			while (scan.hasNext()) {
 				reformatedInfo[i] = scan.next();
 				i++;
@@ -33,6 +34,7 @@ public class LocationInfoExtractor {
 			
 			reformatedInfo[2] = zipCode;
 			reformatedInfo[5] = state;
+			reformatedInfo[8] = metro;
 		}
 		
 		scan.close();
--- SerializeJson.java
+++ SerializeJson.java
@@ -8,19 +8,44 @@ import org.json.JSONObject;
 
 public class SerializeJson {
 
-	public static String getAddress(String jsonObject) throws JSONException{
+	public static String[] getAddress(String jsonObject) throws JSONException{
 		System.out.println(jsonObject);
+		
+		String[] returner = new String[2];
+		String temp1 = "";
+		String temp2 = "";
+		String metro = "";
+		int num1 = 0;
+		int beginIndex = 0;
+		int endIndex = 0;
+		
+		num1 = jsonObject.indexOf("Metropolitan");
+
+		if (num1 != -1) {
+			temp1 = jsonObject.substring(0, num1 + 1);
+			temp2 = jsonObject.substring(num1);
+			beginIndex = temp1.lastIndexOf('"') + 1;
+			endIndex = temp2.indexOf('"') + beginIndex + 18;
+
+			metro = jsonObject.substring(beginIndex, endIndex);
+			System.out.println("This is Metro: " + metro);
+			returner[0] = metro;
+		} else {
+			returner[0] = "";
+		}
+		
 		JSONObject json = new JSONObject(jsonObject);
 		HashMap<String, Object> jsonMap = new HashMap<String, Object>();
-		String returner = "";
 		int tempest = 0;
 		
 		jsonMap = JsonToMap.jsonToMap(json);
 		
 		tempest = jsonMap.get("results").toString().indexOf("geometry");
 		System.out.println(tempest);
-		returner = jsonMap.get("results").toString().substring(20, tempest-1); //"formatted address_ starts at index 2
-		System.out.println(returner);
+		
+		
+		returner[1] = jsonMap.get("results").toString().substring(20, tempest-1); //"formatted address_ starts at index 2		
+		System.out.println("This is returner: " + returner);
 		
 		return returner;
 		
