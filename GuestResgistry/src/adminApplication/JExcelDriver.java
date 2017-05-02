4daba41726da3d0a1b7a8feb4a0bdd5148f58bc4
ConnorDixonWarhawks <dixonc@warhawks.ulm.edu>
2017-05-01 22:56:27 -0500

    Bugfix: Interests now working

--- GuestResgistry/src/adminApplication/JExcelDriver.java
+++ GuestResgistry/src/adminApplication/JExcelDriver.java
@@ -209,8 +209,13 @@ public class JExcelDriver {
 		WritableWorkbook myexcel = Workbook.createWorkbook(file);
 		WritableSheet mysheet = myexcel.createSheet("mySheet", 0);
 		try {
-			for (int i = 1; i < data.size() - 1; i++) {
-				mysheet.addCell(formatData(i - 1, 0, data.get(i).getEmail()));
+			int i = 0;
+			for (VisitorDetails datum : data) {
+				if(datum.getEmail() != null && !datum.getEmail().isEmpty())
+				{
+					mysheet.addCell(formatData(i-1, 0, datum.getEmail()));
+					i++;
+				}
 			}
 		} catch (WriteException e) {
 
